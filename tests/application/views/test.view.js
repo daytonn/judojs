1c865f0a0e5cd67fa17b13a91258d5d5fbc63b7c
Dayton <daytonn@gmail.com>
2010-07-24 03:48:49 -0500

    Made create_project method in Judo to make all the project directorys used by Judo. Now tests use create_project to start the tests.

--- tests/application/views/test.view.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-	JudoApp.test_id = $('#test-element-with-id');
-	console.log(JudoApp.test_id);
-});
--- tests/test.controller.test.js
+++
@@ -1,9 +0,0 @@
-module("Module A");
-
-test("JudoApp Object", function() {
-	ok(isTypeof())
-});
-
-test("second test within module", function() {
-  ok( true, "all pass" );
-});
