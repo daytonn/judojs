9a878a81822d559ef1e422ed609c9547cba33dd7
Dayton <daytonn@gmail.com>
2010-08-24 09:29:40 -0500

    Removed old temp files

--- tests/.tmp.JudoApp.module.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	JudoApp.test_id.click(function() {
-		alert('this is a dummy function');
-		alert(JudoApp.test_model.some_data_member);
-	});
-});
--- tests/.tmp.testmodule.js
+++
@@ -1,6 +0,0 @@
-/Volumes/Storage/Development/Judo/tests/fixtures/plugins/test.plugin.js/Volumes/Storage/Development/Judo/tests/fixtures/elements/test.elements.js/Volumes/Storage/Development/Judo/tests/fixtures/models/test.model.js$(document).ready(function() {
-	JudoApp.test_id.click(function() {
-		alert('this is a dummy function');
-		alert(JudoApp.test_model.some_data_member);
-	});
-});
