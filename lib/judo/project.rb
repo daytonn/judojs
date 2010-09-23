f4cd3047fd77576f51cb208a6d387e79b93e23f6
Dayton Nolan <daytonn@Dayton-Nolans-Mac-Pro.local>
2010-09-23 17:58:05 -0500

    Changed references to @project_path to Judo::Configuration.project_path

--- lib/judo/project.rb
+++ lib/judo/project.rb
@@ -40,7 +40,6 @@ module Judo
     end
     
     def update
-      raise "judo.conf does not exist" unless File.exists? "#{@project_path}/judo.conf"
       Judo::Configuration.load_config
       
       get_modules
@@ -50,11 +49,11 @@ module Judo
     end
 
     def create_project_directories
-      puts "#{@directory} created" unless File.exists? "#{@project_path}"
-      Dir.mkdir "#{@project_path}" unless File.exists? "#{@project_path}"
+      puts "#{@directory} created" unless File.exists? "#{Judo::Configuration.project_path}"
+      Dir.mkdir "#{Judo::Configuration.project_path}" unless File.exists? "#{Judo::Configuration.project_path}"
       
       manifest.each do |folder|
-        Dir.mkdir "#{@project_path}#{folder}" unless File.exists? "#{@project_path}#{folder}"
+        Dir.mkdir "#{Judo::Configuration.project_path}#{folder}" unless File.exists? "#{Judo::Configuration.project_path}#{folder}"
       end
     end
     
@@ -70,9 +69,9 @@ module Judo
     end
 
     def create_conf_file
-      conf_file = File.new("#{@project_path}judo.conf", "w+")
+      conf_file = File.new("#{Judo::Configuration.project_path}judo.conf", "w+")
       conf_content = <<-CONF
-project_path: #{@project_path}
+project_path: #{Judo::Configuration.project_path}
 name: #{Judo::Configuration.name}
 output: #{Judo::Configuration.output}
 #autoload: ['lib/file']
@@ -83,29 +82,29 @@ output: #{Judo::Configuration.output}
     end
     
     def import_javascripts
-      File.copy "#{Judo.base_directory}/repository/judo/tests/index.html", "#{@project_path}tests"
-      File.copy "#{Judo.base_directory}/repository/judo/tests/judo.test.js", "#{@project_path}tests"
-      File.copy "#{Judo.base_directory}/repository/judo/tests/judo.utilities.test.js", "#{@project_path}tests"
+      File.copy "#{Judo.base_directory}/repository/judo/tests/index.html", "#{Judo::Configuration.project_path}tests"
+      File.copy "#{Judo.base_directory}/repository/judo/tests/judo.test.js", "#{Judo::Configuration.project_path}tests"
+      File.copy "#{Judo.base_directory}/repository/judo/tests/judo.utilities.test.js", "#{Judo::Configuration.project_path}tests"
     end
     
     def create_judo_lib_file
       judo_lib_secretary = Sprockets::Secretary.new(
         :root         => "#{Judo.base_directory}",
-        :asset_root   => "#{@project_path}",
+        :asset_root   => "#{Judo::Configuration.project_path}",
         :load_path    => ["repository"],
         :source_files => ["repository/judo/core/judo.js"]
       )
       
       @judo_lib_file = judo_lib_secretary.concatenation
-      @judo_lib_file.save_to "#{@project_path}lib/judo.js"
+      @judo_lib_file.save_to "#{Judo::Configuration.project_path}lib/judo.js"
       
       puts "#{@directory}lib/judo.js created"
     end
     
     def create_judo_application_file      
-      filename = "#{@project_path}application/#{Judo::Configuration.app_filename}.js"
+      filename = "#{Judo::Configuration.project_path}application/#{Judo::Configuration.app_filename}.js"
       
-      puts File.exists?("#{@project_path}application/#{Judo::Configuration.app_filename}.js") ? "application/#{Judo::Configuration.app_filename}.js updated" : "application/#{Judo::Configuration.app_filename}.js created"
+      puts File.exists?("#{Judo::Configuration.project_path}application/#{Judo::Configuration.app_filename}.js") ? "application/#{Judo::Configuration.app_filename}.js updated" : "application/#{Judo::Configuration.app_filename}.js created"
       File.open(filename, "w+") do |file|
         file << "//-- Judo #{Time.now.to_s}  --//\n"
         file << @judo_lib_file
@@ -115,21 +114,21 @@ output: #{Judo::Configuration.output}
     end
     
     def update_application_file
-      message = File.exists?("#{@project_path}application/#{Judo::Configuration.app_filename}.js") ? "application/#{Judo::Configuration.app_filename}.js updated" : "application/#{Judo::Configuration.app_filename}.js created"      
+      message = File.exists?("#{Judo::Configuration.project_path}application/#{Judo::Configuration.app_filename}.js") ? "application/#{Judo::Configuration.app_filename}.js updated" : "application/#{Judo::Configuration.app_filename}.js created"      
       
       content = String.new
       content << "/* Judo #{Time.now.to_s} */\n"
       content << "//= require \"../lib/judo.js\"\n\n"
       content << "\nvar #{Judo::Configuration.name} = new JudoApplication();"
       
-      filename = "#{@project_path}application/#{Judo::Configuration.app_filename}.js"
+      filename = "#{Judo::Configuration.project_path}application/#{Judo::Configuration.app_filename}.js"
       File.open(filename, "w+") do |file|
         file << content
       end
       
       judo_lib_secretary = Sprockets::Secretary.new(
         :root         => "#{Judo.base_directory}",
-        :asset_root   => "#{@project_path}",
+        :asset_root   => "",
         :load_path    => ["repository"],
         :source_files => ["#{filename}"]
       )
@@ -143,7 +142,7 @@ output: #{Judo::Configuration.output}
     def get_modules
       @modules = Array.new
       
-      entries = Dir.entries "#{@project_path}modules"
+      entries = Dir.entries "#{Judo::Configuration.project_path}modules"
       entries.each do |file|
         @modules.push file unless file.match(/^\./)
       end
@@ -192,18 +191,18 @@ output: #{Judo::Configuration.output}
     end
     
     def create_module_file(module_file, module_name)
-        module_src = "#{@project_path}modules/#{module_file}"
+        module_src = "#{Judo::Configuration.project_path}modules/#{module_file}"
         
         judo_lib_secretary = Sprockets::Secretary.new(
           :root         => "#{Judo.base_directory}",
-          :asset_root   => "#{@project_path}",
+          :asset_root   => "#{Judo::Configuration.project_path}",
           :load_path    => ["repository"],
           :source_files => ["#{module_src}"]
         )
 
         module_file = judo_lib_secretary.concatenation
-        message = File.exists?("#{@project_path}application/#{module_name}.js") ? "application/#{module_name}.js updated" : "application/#{module_name}.js created"
-        module_file.save_to "#{@project_path}application/#{module_name}.js"
+        message = File.exists?("#{Judo::Configuration.project_path}application/#{module_name}.js") ? "application/#{module_name}.js updated" : "application/#{module_name}.js created"
+        module_file.save_to "#{Judo::Configuration.project_path}application/#{module_name}.js"
         judo_lib_secretary.install_assets
 
         puts message
